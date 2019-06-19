9e4de903c8d6e7b892607dd89b73897a5c949721
fengyiwan <ily19910525fyw@126.com>
2019-06-19 13:50:02 +0800

    Initial commit

---
+++ MyLogger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+
+"""
+# Project       :   tools
+# File          :   MyLogger.py
+# Author        :   fengyiwan
+# Email         :   ily19910525fyw@126.com
+# Time          :   2019/6/19 13:42
+# Software      :   PyCharm
+# Description   :   自定义Logger类
+"""
+
+import logging
+
+if __name__ == '__main__':
+    print("Hello,Python logger!")
+
